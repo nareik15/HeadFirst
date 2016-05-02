fe9e7c3ed2ba1f208e0157be1dbb94bb0a61f01b
Kieran Doherty <nareik15@gmail.com>
2016-05-02 23:53:27 +0100

    Commit to begin syncing with laptop

---
+++ HeadFirstJava/src/other/MultiplesTest.java
@@ -0,0 +1,23 @@
+package other;
+
+public class MultiplesTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test myTest = new Test();
+		
+		System.out.println(myTest.name);
+		
+	}
+
+}
+
+// only one class (the one including "main"?) can be public...
+
+class Test {
+	
+	String name = "tester";
+	
+	
+	
+}
